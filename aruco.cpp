c393e3893b805408b23f4cd9b9a0a085b9663896
Ammar Ajmal <ammarajml@gmail.com>
2022-11-23 12:18:13 +0900

    createArucoMarkers() - to generate marker files

--- aruco.cpp
+++ aruco.cpp
@@ -1,6 +1,16 @@
 // OpenCv 4.2.0
+#include "opencv2/core.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/calib3d.hpp"
 #include "opencv2/opencv.hpp"
-#include <opencv2/aruco.hpp>
+#include "opencv2/aruco.hpp"
+
+#include <sstream>
+#include <iostream>
+#include <fstream>
+
 using namespace cv;
 using namespace std;
 int markNum(1);
@@ -12,6 +22,18 @@ Mat markerGenerator(int markNum){
     aruco::drawMarker(dictionary, markNum, 200, markerImage, 1);
     imwrite("marker0.png", markerImage);
     return markerImage;
+}
+
+void createArucoMarkers(){
+    Mat outputMarker;
+    Ptr<aruco::Dictionary> markerDictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
+    for (int i = 0; i<50; i++){
+        aruco::drawMarker(markerDictionary, i, 500, outputMarker, 1);
+        ostringstream convert;
+        string imageName = "Marker_4x4_";
+        convert << imageName << i << ".jpg";
+        imwrite(convert.str(), outputMarker);
+    }
 }
 
 
@@ -41,8 +63,9 @@ void markerDetector(int camera){
 
 int main(int, char**)
 {
-    markerGenerator(0);
-    markerDetector(2);
+    createArucoMarkers();
+    // markerGenerator(0);
+    // markerDetector(2);
 
 
     return 0;
