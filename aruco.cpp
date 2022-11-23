a2b20b8ebea7ba477f0eeecc53c166a7a1b9f83f
Ammar Ajmal <ammarajml@gmail.com>
2022-11-23 14:24:02 +0900

    Camera Calibration - completed!

--- aruco.cpp
+++ aruco.cpp
@@ -30,7 +30,8 @@ void createKnownBoardPosition(Size boardSize, float squareEdgeLength, vector<Poi
 void getChessboardCorners(vector<Mat> images, vector<vector<Point2f>>& allFoundCorners, bool showResults = false){
     for  (vector<Mat>::iterator iter = images.begin(); iter != images.end(); iter++){
         vector<Point2f> pointBuf;
-        bool found = findChessboardCorners(*iter, Size(9,6), pointBuf, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE);
+        bool found = findChessboardCorners(*iter, Size(9,6), pointBuf, CALIB_CB_ADAPTIVE_THRESH);
+        // bool found = findChessboardCorners(*iter, Size(9,6), pointBuf, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE); // for fast speed
         
         if (found){
             allFoundCorners.push_back(pointBuf);
@@ -87,9 +88,121 @@ void markerDetector(int camera){
     }
 }
 
+void cameraCalibration(vector<Mat> calibrationImages, Size boardSize, float squareEdgeLength, Mat& cameraMatrix, Mat& distanceCoefficients){
+    vector<vector<Point2f>> checkerboardImageSpacePoints;
+    getChessboardCorners(calibrationImages, checkerboardImageSpacePoints, false);
+    vector<vector<Point3f>> worldSpaceCornerPoints(1);
+    createKnownBoardPosition(boardSize, squareEdgeLength, worldSpaceCornerPoints[0]);
+    worldSpaceCornerPoints.resize(checkerboardImageSpacePoints.size(), worldSpaceCornerPoints[0]);
+    vector<Mat> rVectors, tVectors;
+    distanceCoefficients = Mat::zeros(8, 1, CV_64F);
+    calibrateCamera(worldSpaceCornerPoints, checkerboardImageSpacePoints, boardSize, cameraMatrix, distanceCoefficients, rVectors, tVectors);
+}
 
-int main(int, char**)
+// int cameraCalibrationWebcam(){
+//     Mat frame;
+//     Mat drawToFrame;
+//     Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
+//     Mat distanceCoefficients;
+//     vector<Mat> savedImages;
+//     vector<vector<Point2f>> markerCorners, rejectedCandidates;
+//     VideoCapture vid(2);
+//     if (!vid.isOpened()){
+//         return 0;
+
+//     }
+//     int framesPerSecond = 30;
+//     namedWindow("webcam", WINDOW_AUTOSIZE);
+//     while (true){
+//         if (!vid.read(frame)){
+//             break;
+//         }
+//         vector<Vec2f> foundPoints;
+//         bool found = false;
+
+//         found = findChessboardCorners(frame, chessboardDimensions, foundPoints, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE | CALIB_CB_FAST_CHECK);
+//         frame.copyTo(drawToFrame);
+//         drawChessboardCorners(drawToFrame, chessboardDimensions, foundPoints, found);
+//         if (found){
+//             imshow("webcam", drawToFrame);
+//         }
+//         else{
+//             imshow("webcam", frame);
+//         }
+//         char character = waitKey(1000/framesPerSecond);
+//             // 1. Save an image(Space Key) -- if we see a valid checkerboard image
+//             // 2. Start Camera clibration (Enter Key) -- if we wanted to start camera calibration so long as we have enough images like 10 or 15
+//             // 3. Exit(Escape Key)
+//         switch (character) {
+//             case ' ' :{
+//                 //saving image
+//                 if (found){
+//                     Mat temp;
+//                     frame.copyTo(temp);
+//                     savedImages.push_back(temp);
+//                 }
+//                 break;
+
+//             }
+//             case 13:{
+//                 // start calibration
+//                 if (savedImages.size() > 15) {
+//                     cameraCalibration(savedImages, chessboardDimensions, calibrationSquareDimension, cameraMatrix, distanceCoefficients);
+
+//                 }
+//                 break;
+//             }
+            
+//             case 27:{
+//                 //exit 
+//                 return 0;
+//                 break;
+//             }
+            
+//         }
+
+
+//     }
+
+// }
+
+bool saveCameraCalibration(string name, Mat cameraMatrix, Mat distanceCoefficients){
+    ofstream outstream(name);
+    if (outstream){
+        uint16_t rows = cameraMatrix.rows;
+        uint16_t columns = cameraMatrix.cols;
+
+        for (int r = 0; r < rows; r++){
+            for(int c = 0; c< columns ; c++){
+                double value = cameraMatrix.at<double>(r,c);
+                outstream << value << endl;
+
+            }
+        }
+        rows = distanceCoefficients.rows;
+        columns = distanceCoefficients.cols;
+        for (int r = 0; r < rows; r++){
+            for(int c = 0; c< columns ; c++){
+                double value = distanceCoefficients.at<double>(r,c);
+                outstream << value << endl;
+
+            }
+        }
+        outstream.close();
+        return true;
+    }
+    return false;
+}
+
+int mainCalib()
 {
+    // cameraCalibrationWebcam();
+
+    // createArucoMarkers();
+    // markerGenerator(0);
+    // markerDetector(2);
+
+
     Mat frame;
     Mat drawToFrame;
     Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
@@ -120,9 +233,47 @@ int main(int, char**)
             imshow("webcam", frame);
         }
         char character = waitKey(1000/framesPerSecond);
+            // 1. Save an image(Space Key) -- if we see a valid checkerboard image
+            // 2. Start Camera clibration (Enter Key) -- if we wanted to start camera calibration so long as we have enough images like 10 or 15
+            // 3. Exit(Escape Key)
+        switch (character) {
+            case ' ' :{
+                //saving image
+                if (found){
+                    Mat temp;
+                    frame.copyTo(temp);
+                    savedImages.push_back(temp);
+                }
+                break;
+
+            }
+            case 13:{
+                // start calibration
+                if (savedImages.size() > 15) {
+                    cameraCalibration(savedImages, chessboardDimensions, calibrationSquareDimension, cameraMatrix, distanceCoefficients);
+                    saveCameraCalibration("my_camera_calibration", cameraMatrix, distanceCoefficients);
+
+                }
+                break;
+            }
+            
+            case 27:{
+                //exit 
+                return 0;
+                break;
+            }
+            
+        }
+
+
     }
-    // createArucoMarkers();
-    // markerGenerator(0);
-    // markerDetector(2);
+
+
+    return 0;
+}
+
+int main(int, char**)
+{
+    mainCalib();
     return 0;
 }
