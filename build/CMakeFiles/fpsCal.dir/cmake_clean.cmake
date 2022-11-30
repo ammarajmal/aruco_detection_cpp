f12cffe8961f121f6e3cad56ca8724b3577ae022
Ammar Ajmal <ammarajml@gmail.com>
2022-12-01 02:44:03 +0900

    rearranging files.

---
+++ build/CMakeFiles/fpsCal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fpsCal.dir/src/fpsCal.cpp.o"
+  "fpsCal"
+  "fpsCal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fpsCal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
